e1a2827a2b62a95bcad0f882028f71637db8f264
Maksim Sech <maksim.sech@gmail.com>
2024-01-11 19:30:20 +0100

    add domain names

--- terrraform/vercel.tf
+++ terrraform/vercel.tf
@@ -10,3 +10,13 @@ resource "vercel_project" "this" {
     deployment_type = "standard_protection"
   }
 }
+
+resource "vercel_project_domain" "dev" {
+  project_id = vercel_project.this.id
+  domain     = "your-spot-dev.vercel.app"
+}
+
+resource "vercel_project_domain" "prod" {
+  project_id = vercel_project.this.id
+  domain     = "your-spot.vercel.app"
+}
