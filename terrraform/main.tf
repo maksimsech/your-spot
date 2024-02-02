9a847bdf81ce63b15f9ff684ef853b29a189d41c
Maksim Sech <maksim.sech@gmail.com>
2024-02-02 17:51:27 +0100

    mongodb initial setup

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -4,7 +4,29 @@ terraform {
       source  = "vercel/vercel"
       version = "1.0.0"
     }
+
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "1.14.0"
+    }
   }
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodbatlas_public_key
+  private_key = var.mongodbatlas_private_key
+}
+
+variable "mongodbatlas_public_key" {
+  type        = string
+  nullable    = false
+  description = "MongoDB Atlas public key"
+}
+
+variable "mongodbatlas_private_key" {
+  type        = string
+  nullable    = false
+  description = "MongoDB Atlas private key"
 }
 
 provider "vercel" {
@@ -12,6 +34,7 @@ provider "vercel" {
 }
 
 variable "vercel_api_key" {
+  type        = string
   nullable    = false
   description = "Vercel api key"
 }
