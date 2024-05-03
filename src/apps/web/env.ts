65c6db685917d2adb72e43b36c7c2312da97a7b6
Maksim Sech <maksim.sech@gmail.com>
2024-05-03 18:41:45 +0200

    Update env.ts

--- src/apps/web/env.ts
+++ src/apps/web/env.ts
@@ -5,7 +5,6 @@ const envSchema = z.object({
     SYSTEM_REFRESH_FF_CACHE_API_KEY: z.string().min(1),
 })
 
-
 const {
     SYSTEM_REFRESH_FF_CACHE_API_KEY: refreshFFCacheApiKey,
 } = envSchema.parse(process.env)
